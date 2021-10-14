345e036449526e62f5e884642bb5417377511fa2
Michael Shustov <shustovmm@gmail.com>
2021-10-14 21:26:21 +0300

    - add IBOutlet properties - add IBAction function

--- WarCardGameUIKit/ViewController.swift
+++ WarCardGameUIKit/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var playerScore: UILabel!
+    
+    @IBOutlet weak var cpuScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealButton(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 

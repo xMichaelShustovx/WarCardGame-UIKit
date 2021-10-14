ca0b5df1b9714bef216f72f01102f88e31c3cb2e
Michael Shustov <shustovmm@gmail.com>
2021-10-14 21:56:17 +0300

    - finish implementing dealButton logic

--- WarCardGameUIKit/ViewController.swift
+++ WarCardGameUIKit/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rightImageView: UIImageView!
     
-    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var playerScoreLabel: UILabel!
     
-    @IBOutlet weak var cpuScore: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
+    var playerScore = 0
+    var cpuScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +27,25 @@ class ViewController: UIViewController {
 
     @IBAction func dealButton(_ sender: Any) {
         
+        let leftCard = Int.random(in: 2...14)
+        let rightCard = Int.random(in: 2...14)
         
+        leftImageView.image = UIImage(named: "card\(leftCard)")
         
+        rightImageView.image = UIImage(named: "card\(rightCard)")
+        
+        if leftCard > rightCard {
+            
+            playerScore += 50
+            
+            playerScoreLabel.text = String(playerScore)
+        }
+        else if leftCard < rightCard {
+            
+            cpuScore += 50
+            
+            cpuScoreLabel.text = String(cpuScore)
+        }
     }
-    
 }
 
